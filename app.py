fc4f55ad67ac69987fe8f71ef7782773457d92d3
dai28-o <archdai28@gmail.com>
2025-09-25 21:29:16 +0900

    Add completion checkbox to tasks

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, world!'
+    return send_file('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
